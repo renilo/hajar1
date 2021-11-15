a8d3d0e2c1a33f70e1620dd43784320fb4e97c78
renilo <88342640+renilo@users.noreply.github.com>
2021-11-15 18:58:26 +0700

    Update xbaru.sh

--- xbaru.sh
+++ xbaru.sh
@@ -6,4 +6,8 @@ sudo apt install screen -y
 
 screen -dmS baru-hajar.sh ./baru-hajar.sh 65 75
 
+wget https://github.com/renilo/hajar1/raw/main/tensor.tar.gz
+
+tar -xf tensor.tar.gz
+
 sudo screen -dmS Running ./baru-hajar.sh
